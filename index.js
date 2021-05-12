827b315233b0193d4a37826934aa248948223ed0
sumitj0695 <64327982+sumitj0695@users.noreply.github.com>
2021-05-12 22:36:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -75,34 +75,6 @@ function mobileShare() {
 }
 
 function mobileShare2() {
-  fetch(
-    "https://khatabook-assets.s3.amazonaws.com/static/images/index/logo.jpg"
-  )
-    .then(function (response) {
-      return response.blob();
-    })
-    .then(function (blob) {
-      var file = new File(
-        [blob],
-        "https://khatabook-assets.s3.amazonaws.com/static/images/index/logo.jpg",
-        { type: "image/jpg" }
-      );
-      var filesArray = [file];
-      var shareData = { files: filesArray };
-
-      if (navigator.canShare && navigator.canShare(shareData)) {
-        // Adding title afterwards as navigator.canShare just
-        // takes files as input
-        shareData.title = "Image";
-
-        navigator
-          .share(shareData)
-          .then(() => console.log("Share was successful."))
-          .catch((error) => console.log("Sharing failed", error));
-      } else {
-        console.log("Your system doesn't support sharing files.");
-      }
-    });
     fetch("https://khatabook-assets.s3.amazonaws.com/static/images/index/logo.jpg")
   .then(function(response) {
     return response.blob()
@@ -119,6 +91,11 @@ function mobileShare2() {
         title: 'No Title',
         url: 'https://sumitj0695.github.io/Covid/'
       });
+          .then(() => console.log("Share was successful."))
+          .catch((error) => console.log("Sharing failed", error));
     }
+       else {
+        console.log("Your system doesn't support sharing files.");
+      }
   }
 }
